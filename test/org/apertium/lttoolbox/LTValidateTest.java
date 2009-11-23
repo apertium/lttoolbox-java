6723d527f9296f11ba015078fc839a7bac167398
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2009-11-23 13:50:42 +0000

    oops, forgot dix/ACX validation
    git-svn-id: file:///home/apertium/svn-mirror/apertium-sf-net/trunk/lttoolbox-java@16953 2c8c2297-680c-4500-a755-649f80e260dd

---
+++ test/org/apertium/lttoolbox/LTValidateTest.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.apertium.lttoolbox;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author Jacob Nordfalk
+ */
+public class LTValidateTest  extends TestCase {
+
+  public void testDixvalidation() throws Exception {
+     assertTrue(LTValidate.validateDix("testdata/short.dix"));
+     assertFalse(LTValidate.validateDix("testdata/short-invalid.dix"));
+     assertFalse(LTValidate.validateDix("nonexisant.dix"));
+  }
+
+  public void testAcxvalidation() throws Exception {
+     assertTrue(LTValidate.validateAcx("testdata/apertium-fr-es.fr.acx"));
+  }
+
+}
