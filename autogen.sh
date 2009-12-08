d05020dbc42cc94baeb3f8628bd1604a1dcc80b7
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2009-12-08 09:44:29 +0000

    git-svn-id: file:///home/apertium/svn-mirror/apertium-sf-net/trunk/lttoolbox-java@17472 2c8c2297-680c-4500-a755-649f80e260dd

--- autogen.sh
+++ autogen.sh
@@ -2,6 +2,9 @@
 
 echo 
 echo "No automatic generation script needed"
-echo "Make sure Java (JDK version 1.6 or newer) and ant is installed"
-echo "type 'make; sudo make install'"
+echo "Make sure Java (JDK version 1.6 or newer) and ant is installed and type:"
+echo "	make"
+echo "	sudo make install"
 echo 
+echo "(other options are: 'make tests' to do regression testing of the package, and 'make clean' to cleanup)"
+
