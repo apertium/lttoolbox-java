43675b69a1d626ec47069d26880daf538821ddc0
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2009-12-11 15:20:37 +0000

    added old files needed for support of transfer
    git-svn-id: file:///home/apertium/svn-mirror/apertium-sf-net/trunk/lttoolbox-java@17607 2c8c2297-680c-4500-a755-649f80e260dd

---
+++ src/org/apertium/lttoolbox/transfer/PatternStore.java
@@ -0,0 +1,13 @@
+package org.apertium.lttoolbox.transfer;
+
+//import org.apache.commons.collections15.multimap.MultiHashMap;
+
+import java.util.List;
+
+/**
+ * Created by Nic Cottrell, Jan 27, 2009 5:02:30 PM
+ */
+
+public class PatternStore {//extends MultiHashMap<Integer, List<Integer>> {
+
+}
