1bf6e09b3296602920faf9444438901f73ef7ace
Francis M. Tyers <ftyers@prompsit.com>
2009-12-22 11:25:39 +0000

    now prefixes will probably work
    git-svn-id: file:///home/apertium/svn-mirror/apertium-sf-net/trunk/lttoolbox-java@18019 2c8c2297-680c-4500-a755-649f80e260dd

--- Makefile
+++ Makefile
@@ -2,13 +2,17 @@ all:
 	ant -quiet jar
 
 install: all
-	cp dist/lttoolbox.jar /usr/local/share/apertium/
-	cp -a lt-proc-j lt-expand-j lt-comp-j lt-validate-j /usr/local/bin/
+	PREFIX=`cat prefix`;
+ifeq ($PREFIX, "")
+	@echo "Run autogen.sh"
+else
+	cp dist/lttoolbox.jar $(PREFIX)/share/apertium/
+	cp -a lt-proc-j lt-expand-j lt-comp-j lt-validate-j $(PREFIX)/bin/
+endif
 
 tests:
 	ant -quiet test
 
 clean:
 	ant -quiet clean
-
-
+	rm prefix;
--- autogen.sh
+++ autogen.sh
@@ -1,11 +1,23 @@
 #! /bin/sh
 
-echo 
-echo "No automatic generation script needed"
-echo "Make sure Java (JDK version 1.6 or newer) and ant is installed and type:"
-echo "(Mac users: First type: export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6.0/Home)"
-echo "	make"
-echo "	sudo make install"
-echo 
-echo "(other options are: 'make tests' to do regression testing of the package, and 'make clean' to cleanup)"
+#echo 
+#echo "No automatic generation script needed"
+#echo "Make sure Java (JDK version 1.6 or newer) and ant is installed and type:"
+#echo "(Mac users: First type: export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6.0/Home)"
+#echo "	make"
+#echo "	sudo make install"
+#echo 
+#echo "(other options are: 'make tests' to do regression testing of the package, and 'make clean' to cleanup)"
+
+echo -n "Setting prefix to ... ";
+echo $1 | grep '\-\-prefix' > /dev/null;
+
+if [ $? -eq 0 ]; then 
+	echo $1 | cut -f2 -d'=' > prefix;
+	cat prefix;
+else
+	echo '/usr/local' > prefix;
+	cat prefix;
+fi
+
 
