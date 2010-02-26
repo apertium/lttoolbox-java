54fb391c23637d7c35f14696331c3c1267c075ab
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2010-02-26 23:14:49 +0000

    much mor testing
    git-svn-id: file:///home/apertium/svn-mirror/apertium-sf-net/trunk/lttoolbox-java@20367 2c8c2297-680c-4500-a755-649f80e260dd

---
+++ test/org/apertium/transfer/TransferEnCyTest.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.apertium.transfer;
+
+/**
+ *
+ * @author Jacob Nordfalk
+ */
+public class TransferEnCyTest extends TransferEoEnTest {
+
+  {
+    dir = "en-cy";
+    tlx = "cy-en."+dir+".t1x";
+    outFile = "transferoutput-"+dir+".t1x-malgranda.txt";
+    t1xFile = folder+"apertium-"+tlx;
+    binFile = folder+dir+".t1x.bin";
+  }
+
+}
