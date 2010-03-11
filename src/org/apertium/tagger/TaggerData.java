243e89b945b7e89464bc195ac3ab63aa93e12344
Jim O'Regan <joregan@gmail.com>
2010-03-11 00:09:44 +0000

    partial conversion of read()
    git-svn-id: file:///home/apertium/svn-mirror/apertium-sf-net/trunk/lttoolbox-java@20699 2c8c2297-680c-4500-a755-649f80e260dd

--- src/org/apertium/tagger/TaggerData.java
+++ src/org/apertium/tagger/TaggerData.java
@@ -21,6 +21,10 @@ package org.apertium.tagger;
 import java.util.ArrayList;
 import java.util.Set;
 import java.util.Map;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.IOException;
+import org.apertium.lttoolbox.Compression;
 
 /**
  *
@@ -39,9 +43,126 @@ public class TaggerData {
     private int N;
     private int M;
 /*
-  vector<wstring> prefer_rules;
   Collection output;
   double **a;
   double **b;
 */
+
+
+    public ArrayList<TForbidRule> getForbidRules() {
+        return forbid_rules;
+    }
+
+    public void setForbidRules(ArrayList<TForbidRule> rules) {
+        this.forbid_rules = rules;
+    }
+
+    public void read (InputStream in) throws IOException {
+      // open class
+      int val = 0;
+      for (int i = Compression.multibyte_read(in); i != 0; i--) {
+          val += Compression.multibyte_read(in);
+          open_class.add(val);
+      }
+   
+      // forbid_rules
+      for (int i = Compression.multibyte_read(in); i != 0; i--) {
+          TForbidRule aux = new TForbidRule();
+          aux.tagi = Compression.multibyte_read(in);
+          aux.tagj = Compression.multibyte_read(in);
+          forbid_rules.add(aux);
+      }
+
+      // array_tags
+      for (int i = Compression.multibyte_read(in); i != 0; i--) {
+          array_tags.add(Compression.String_read(in));
+      }
+
+      // tag_index
+      for (int i = Compression.multibyte_read(in); i != 0; i--) {
+          String tmp = Compression.String_read(in);
+          // FIXME
+          // tag_index[tmp] = Compression.multibyte_read(in);
+      }
+  
+      // enforce_rules
+      for (int i = Compression.multibyte_read(in); i != 0; i--) {
+          TEnforceAfterRule aux = new TEnforceAfterRule();
+          aux.tagi = Compression.multibyte_read(in);
+          for (int j = Compression.multibyte_read(in); j != 0; j--) {
+              aux.tagsj.add(Compression.multibyte_read(in));
+          }
+          enforce_rules.add(aux);
+      }
+
+      // prefer_rules
+      for (int i = Compression.multibyte_read(in); i != 0; i--) {
+          prefer_rules.add(Compression.String_read(in));
+      }
+
+      // constants
+      constants.read(in);
+
+      // output
+      // FIXME
+      //output.read(in);
+
+      // dimensions
+      N = Compression.multibyte_read(in);
+      M = Compression.multibyte_read(in);
+
+/*
+  a = new double * [N];
+  b = new double * [N];
+  for(int i = 0; i != N; i++)
+  {
+    a[i] = new double[N];
+    b[i] = new double[M];
+  }
+   
+  // read a
+  for(int i = 0; i != N; i++)
+  {
+    for(int j = 0; j != N; j++)
+    {
+      a[i][j] = EndianDoubleUtil::read(in);
+    }
+  }
+
+  // initializing b matix
+  for(int i = 0 ; i != N; i++)
+  {
+    for(int j = 0; j != M; j++)
+    {
+      b[i][j] = ZERO;
+    }
+  }
+*/
+
+      // read nonZERO values of b
+      int nval = Compression.multibyte_read(in);
+
+      for (; nval != 0; nval--) {
+          int i = Compression.multibyte_read(in);
+          int j = Compression.multibyte_read(in);
+          //b[i][j] = EndianDoubleUtil.read(in);
+      }
+
+      // read pattern list
+      plist.read(in);
+    
+      // read discards on ambiguity
+      //discard.clear();
+
+      int limit = Compression.multibyte_read(in);
+//      if (feof(in)) {
+//          return;
+//      }
+  
+      for (int i = 0; i < limit; i++) {
+          //discard.add(Compression.String_read(in));
+      }
+
+
+    }
 }
