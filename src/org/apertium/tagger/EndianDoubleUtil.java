2db11b75bfdd98dd1ea41477d5a3e260d276afbd
Jim O'Regan <joregan@gmail.com>
2010-03-11 00:50:58 +0000

    EndianDoubleUtil.java - but endianness is not an issue, just bytes to double
    git-svn-id: file:///home/apertium/svn-mirror/apertium-sf-net/trunk/lttoolbox-java@20700 2c8c2297-680c-4500-a755-649f80e260dd

---
+++ src/org/apertium/tagger/EndianDoubleUtil.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2010 Jimmy O'Regan
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
+
+package org.apertium.tagger;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+/**
+ * This class is a misnomer, as endianness is not an issue
+ * @author jimregan
+ */
+public class EndianDoubleUtil {
+    public double read (InputStream in) throws IOException {
+        byte[] b = new byte[8];
+        in.read(b);
+        ByteBuffer bb = ByteBuffer.wrap(b);
+        return bb.getDouble();
+    }
+}
