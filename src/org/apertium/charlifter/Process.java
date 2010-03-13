8c413d2b09d61687b5318d81dc964304725f8cd8
Jim O'Regan <joregan@gmail.com>
2010-03-13 17:08:11 +0000

    start converting runtime processing
    git-svn-id: file:///home/apertium/svn-mirror/apertium-sf-net/trunk/lttoolbox-java@20775 2c8c2297-680c-4500-a755-649f80e260dd

---
+++ src/org/apertium/charlifter/Process.java
@@ -0,0 +1,52 @@
+/*
+# This file is part of Charlifter.
+# Copyright 2008-2009 Kevin P. Scannell <kscanne at gmail dot com>
+#
+#     Charlifter is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     Charlifter is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.apertium.charlifter;
+
+/**
+ *
+ * @author jimregan
+ */
+public class Process {
+    private double[][] features;
+
+    private String diacritics;
+    private String ints;
+    private String extrabd;
+    private int okina; // boolean?
+
+    void init () {
+        this.diacritics = "\\p{M}|[\\x{A8}\\x{B4}\\x{B8}\\x{02B9}-\\x{02DD}]";
+        this.ints = "´'’-";
+        this.extrabd = "a";
+        this.okina = 0;
+    }
+    
+    void init (String lang) {
+        if (lang.equals("sm") || lang.equals("haw")) {
+            this.diacritics = "\\p{M}|[\\x{A8}\\x{B4}\\x{B8}\\x{02B9}-\\x{02DD}]";
+            this.ints = "-";
+            this.extrabd = "'’‘";
+            this.okina = 1;
+        } else {
+            init();
+        }
+    }
+
+
+}
