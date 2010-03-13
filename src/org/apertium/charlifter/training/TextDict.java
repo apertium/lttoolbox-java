61248190beafe762327d771bf447398e88a74cfb
Jim O'Regan <joregan@gmail.com>
2010-03-13 17:06:33 +0000

    add a basic thing to read in a basic wordlist, because Java is so much more verbose than Perl
    git-svn-id: file:///home/apertium/svn-mirror/apertium-sf-net/trunk/lttoolbox-java@20774 2c8c2297-680c-4500-a755-649f80e260dd

---
+++ src/org/apertium/charlifter/training/TextDict.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2010 Jimmy O'Regan
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
+
+package org.apertium.charlifter.training;
+import org.apertium.charlifter.Data;
+import org.apertium.charlifter.Asciify;
+
+/**
+ *
+ * @author jimregan
+ */
+public class TextDict {
+    Data read_clean_dict (String file, Data data) {
+        String[] words;
+        try {
+            words = Wordlist.read(file);
+            for (String word : words) {
+                String asc = Asciify.toascii(word.toLowerCase());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+}
