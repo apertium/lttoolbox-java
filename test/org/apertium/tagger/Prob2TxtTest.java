837186051da93e107f7df9053b5a5b67af9c1f72
Jim O'Regan <joregan@gmail.com>
2010-03-29 22:51:56 +0000

    test file
    git-svn-id: file:///home/apertium/svn-mirror/apertium-sf-net/trunk/lttoolbox-java@21248 2c8c2297-680c-4500-a755-649f80e260dd

---
+++ test/org/apertium/tagger/Prob2TxtTest.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2010 Jimmy O'Regan
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
+
+package org.apertium.tagger;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author jim
+ */
+public class Prob2TxtTest {
+
+    public Prob2TxtTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class Prob2Txt.
+     */
+    @Test
+    public void testMain() throws Exception {
+        System.out.println("main");
+        String[] argv = {"-f", "/home/jim/Devel/apertium-en-es/en-es.prob"};
+        Prob2Txt.main(argv);
+    }
+
+}
