037fe32785819a2688015e96abb60367e376bfaf
Moffassal Hossain <mithu_ihk@users.sourceforge.net>
2010-04-12 19:46:05 +0000

    "Fixed infinite loop at Morphostram.java line 121"
    git-svn-id: file:///home/apertium/svn-mirror/apertium-sf-net/trunk/lttoolbox-java@21742 2c8c2297-680c-4500-a755-649f80e260dd

--- src/org/apertium/tagger/MorphoStream.java
+++ src/org/apertium/tagger/MorphoStream.java
@@ -16,8 +16,8 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
  * 02111-1307, USA.
  */
-
 package org.apertium.tagger;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -33,13 +33,12 @@ import java.io.IOException;
  *
  * @author jimregan
  */
-
-
 /** Class MorphoStream.
  *  This class processes the FST, and
  *  builds the TaggerWord objects managed by the tagger
  */
 public class MorphoStream {
+
     private boolean foundEOF;
     private boolean debug = false;
     private String last_string_tag;
@@ -54,70 +53,64 @@ public class MorphoStream {
     private int ca_kunknown;
     private int ca_tag_keof;
     private int ca_tag_kundef;
-
     private ArrayList<TaggerWord> vwords;
     private InputStream input;
-
     private MatchExe me;
     private TaggerData td;
     private Alphabet alphabet;
     private MatchState ms;
-
     private boolean end_of_file;
     private boolean null_flush;
+    private Map<String, Integer> tag_index;
+    private ConstantManager constants;
 
-    MorphoStream () {
+//    MorphoStream() {
+//    }
+    /**
+     * Constructor
+     * @param ftxt the input stream.
+     */
+    MorphoStream(InputStream ftxt, boolean d, TaggerData t) {
+        // this();
         foundEOF = false;
         alphabet = new Alphabet();
         ca_any_char = alphabet.cast(PatternList.ANY_CHAR);
         ca_any_tag = alphabet.cast(PatternList.ANY_TAG);
 
         null_flush = false;
-        end_of_file = false;
-    }
-
-   /**
-    * Constructor
-    * @param ftxt the input stream.
-    */
-    MorphoStream (InputStream ftxt, boolean d, TaggerData t) {
-        this();
         this.input = ftxt;
         this.td = t;
+        this.end_of_file = false;
 //        this.me = this.td.getPatternList().newMatchExe();
-        
-        ConstantManager constants = td.getConstants();
-        ca_kignorar = constants.getConstant("kIGNORAR");
-        ca_kbarra = constants.getConstant("kBARRA");
-        ca_kdollar = constants.getConstant("kDOLLAR");
-        ca_kbegin = constants.getConstant("kBEGIN");
-        ca_kmot = constants.getConstant("kMOT");
-        ca_kmas = constants.getConstant("kMAS");
-        ca_kunknown = constants.getConstant("kUNKNOWN");
 
-        Map<String, Integer> tag_index = td.getTagIndex();
-        ca_tag_keof = tag_index.get("TAG_kEOF");
-        ca_tag_kundef = tag_index.get("TAG_kUNDEF");
+        this.constants = td.getConstants();
+        this.ca_kignorar = constants.getConstant("kIGNORAR");
+        this.ca_kbarra = constants.getConstant("kBARRA");
+        this.ca_kdollar = constants.getConstant("kDOLLAR");
+        this.ca_kbegin = constants.getConstant("kBEGIN");
+        this.ca_kmot = constants.getConstant("kMOT");
+        this.ca_kmas = constants.getConstant("kMAS");
+        this.ca_kunknown = constants.getConstant("kUNKNOWN");
+
+        this.tag_index = td.getTagIndex();
+        this.ca_tag_keof = tag_index.get("TAG_kEOF");
+        this.ca_tag_kundef = tag_index.get("TAG_kUNDEF");
+        this.vwords = new ArrayList<TaggerWord>();
     }
 
-   /**
-    * Get next word in the input stream
-    * @return The next word in the input stream
-    */
-    TaggerWord get_next_word () throws IOException {
-        // TODO: Remove Jimmy's ugly fix:
-        if (vwords==null) {
-            this.vwords = new ArrayList<TaggerWord>();
-        }
+    /**
+     * Get next word in the input stream
+     * @return The next word in the input stream
+     */
+    TaggerWord get_next_word() throws IOException {
 
-        // If we have a word in the buffer, return it
         if (vwords.size() != 0) {
             TaggerWord word = vwords.get(0);
             vwords.remove(0);
 
             if (word.isAmbiguous()) {
                 List<String> ref = td.getDiscardRules();
-                for (int i=0; i < ref.size(); i++) {
+                for (int i = 0; i < ref.size(); i++) {
                     word.discardOnAmbiguity(ref.get(i));
                 }
             }
@@ -125,26 +118,27 @@ public class MorphoStream {
         }
 
         // FIXME!
-        if(end_of_file)
-        {
+        input.mark(0);  // Mark the current possition of this input stream so we can go back to that possition
+        // bcoz testing input.read()==-1 will advance the pointer/cursor! it will not read
+        if (end_of_file || input.read() == -1) {
             return null;
         }
-
-
+        input.reset();
         // no word in the buffer, so read from input
         int ivwords = 0;
         vwords.add(new TaggerWord());
 
         while (true) {
+
             int symbol = input.read();
             if (symbol == -1 || (null_flush && symbol == '\0')) {
                 this.end_of_file = true;
                 td.setPreferRules(vwords.get(ivwords).add_tag(ca_tag_keof, "", td.getPreferRules()));
                 // word read, use above code to return it
-                return get_next_word ();
+                return get_next_word();
             }
             if (symbol == (int) '^') {
-                readRestOfWord (ivwords);
+                readRestOfWord(ivwords);
                 // word read, use above code to return it
                 return get_next_word();
             } else {
@@ -156,6 +150,7 @@ public class MorphoStream {
                     symbol = (int) '\\';
                 } else {
                     str += (char) symbol;
+
                 }
                 while (symbol != (int) '^') {
                     symbol = input.read();
@@ -167,12 +162,13 @@ public class MorphoStream {
                         return get_next_word();
                     }
                 }
+
             }
         }
 
     }
 
-    void readRestOfWord (int ivwords) throws IOException {
+    void readRestOfWord(int ivwords) throws IOException {
         String str = "";
         while (true) {
             int symbol = input.read();
@@ -181,9 +177,9 @@ public class MorphoStream {
                 if (str.length() > 0) {
                     vwords.get(ivwords).add_ignored_string(str);
 
-                    System.err.println ("Warning (internal): kIGNORE was returned while reading a word");
-                    System.err.println ("Word being read: " + vwords.get(ivwords).get_superficial_form());
-                    System.err.println ("Debug: " + str);
+                    System.err.println("Warning (internal): kIGNORE was returned while reading a word");
+                    System.err.println("Word being read: " + vwords.get(ivwords).get_superficial_form());
+                    System.err.println("Debug: " + str);
                 }
                 td.setPreferRules(vwords.get(ivwords).add_tag(ca_tag_keof, "", td.getPreferRules()));
                 return;
@@ -210,9 +206,9 @@ public class MorphoStream {
                 end_of_file = true;
                 if (str.length() > 0) {
                     vwords.get(ivwords).add_ignored_string(str);
-                    System.err.println ("Warning (internal): kIGNORE was returned while reading a word");
-                    System.err.println ("Word being read: " + vwords.get(ivwords).get_superficial_form());
-                    System.err.println ("Debug: " + str);
+                    System.err.println("Warning (internal): kIGNORE was returned while reading a word");
+                    System.err.println("Word being read: " + vwords.get(ivwords).get_superficial_form());
+                    System.err.println("Debug: " + str);
                 }
                 td.setPreferRules(vwords.get(ivwords).add_tag(ca_tag_keof, "", td.getPreferRules()));
                 return;
@@ -222,7 +218,7 @@ public class MorphoStream {
                 str += (char) symbol;
                 symbol = '\\';
             } else if (symbol == (int) '/') {
-                lrlmClassify (str, ivwords);
+                lrlmClassify(str, ivwords);
                 str = "";
                 ivwords = 0;
                 continue;
@@ -238,27 +234,28 @@ public class MorphoStream {
         }
     }
 
-    void lrlmClassify (String str, int ivwords) {
+    void lrlmClassify(String str, int ivwords) {
         int floor = 0;
         int last_type = -1;
         int last_pos = 0;
+        TaggerWord tw;
 
         this.ms = new MatchState(this.td.getPatternList().me);
         ms.init(this.td.getPatternList().me.getInitial());
-        for (int i=0, limit=str.length(); i!=limit;i++) {
+        for (int i = 0, limit = str.length(); i != limit; i++) {
             if (str.charAt(i) != '<') {
                 if (str.charAt(i) == '+') {
                     //int val = ms.classifyFinals(me.getFinals());
                     int val = ms.classifyFinals();
                     if (val != -1) {
-                        last_pos = i-1;
+                        last_pos = i - 1;
                         last_type = val;
                     }
                 }
                 ms.step(str.toLowerCase().charAt(i), ca_any_char);
             } else {
                 String tag = "";
-                for (int j=i+1;j!=limit;j++) {
+                for (int j = i + 1; j != limit; j++) {
                     if (str.charAt(j) == '\\') {
                         j++;
                     } else if (str.charAt(j) == '>') {
@@ -272,30 +269,31 @@ public class MorphoStream {
                 //int symbol = alphabet(tag);
                 int symbol = alphabet.cast(tag);
                 if (symbol != 0) {
-                    ms.step (symbol, ca_any_tag);
+                    ms.step(symbol, ca_any_tag);
                 } else {
-                    ms.step (ca_any_tag);
+                    ms.step(ca_any_tag);
                 }
             }
             if (ms.size() == 0) {
                 if (last_pos != floor) {
                     td.setPreferRules(vwords.get(ivwords).add_tag(last_type,
-                                                   str.substring(floor, last_pos),
-                                                   td.getPreferRules()));
-                    if (str.charAt(last_pos+1) == '+' && last_pos+1 < limit) {
+                            str.substring(floor, last_pos),
+                            td.getPreferRules()));
+                    if (str.charAt(last_pos + 1) == '+' && last_pos + 1 < limit) {
                         floor = last_pos + 1;
                         last_pos = floor;
                         vwords.get(ivwords).set_plus_cut(true);
-                        if (vwords.size()<=(ivwords+1))
+                        if (vwords.size() <= (ivwords + 1)) {
                             vwords.add(new TaggerWord(true));
+                        }
                         ivwords++;
                         ms.init(me.getInitial());
                     } else {
                         if (debug) {
-                            System.err.println("Warning: There is no coarse tag for the fine tag '" + str.substring(floor) +"'");
+                            System.err.println("Warning: There is no coarse tag for the fine tag '" + str.substring(floor) + "'");
                             System.err.println("         This is because of an incomplete tagset definition or a dictionary error");
                         }
-                        TaggerWord tw = new TaggerWord();
+                        //TaggerWord tw = new TaggerWord();
                         tw = vwords.get(ivwords);
                         td.setPreferRules(tw.add_tag(ca_tag_kundef, str.substring(floor), td.getPreferRules()));
                         vwords.set(ivwords, tw);
@@ -309,40 +307,39 @@ public class MorphoStream {
         if (val == -1) {
             val = ca_tag_kundef;
             if (debug) {
-                System.err.println("Warning: There is no coarse tag for the fine tag '" + str.substring(floor) +"'");
+                System.err.println("Warning: There is no coarse tag for the fine tag '" + str.substring(floor) + "'");
                 System.err.println("         This is because of an incomplete tagset definition or a dictionary error");
             }
-            TaggerWord tw = new TaggerWord();
+            //TaggerWord tw = new TaggerWord();
             tw = vwords.get(ivwords);
             td.setPreferRules(tw.add_tag(val, str.substring(floor), td.getPreferRules()));
             vwords.set(ivwords, tw);
         }
-        
+
     }
 
-   /**
-    * Set up the flag to detect '\0' characters
-    * @param nf the null_flush value
-    */
-    void setNullFlush (boolean nf) {
+    /**
+     * Set up the flag to detect '\0' characters
+     * @param nf the null_flush value
+     */
+    void setNullFlush(boolean nf) {
         null_flush = nf;
     }
 
-   /**
-    * Return true if the last reading is end of file of '\0' when null_flush
-    * is true
-    * @returns the value of end_of_file
-    */
-    boolean getEndOfFile () {
+    /**
+     * Return true if the last reading is end of file of '\0' when null_flush
+     * is true
+     * @returns the value of end_of_file
+     */
+    boolean getEndOfFile() {
         return end_of_file;
     }
 
-   /**
-    * Sets a new value for the end_of_file_flag
-    * @param eof the new value for end_of_file
-    */
-    void setEndOfFile (boolean eof) {
+    /**
+     * Sets a new value for the end_of_file_flag
+     * @param eof the new value for end_of_file
+     */
+    void setEndOfFile(boolean eof) {
         end_of_file = eof;
     }
-
 }
