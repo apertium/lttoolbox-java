6d2313ba054aa9cf48a40299b9a4649a5881599b
Pasquale Minervini <p.minervini@gmail.com>
2010-06-24 12:22:32 +0000

    git-svn-id: file:///home/apertium/svn-mirror/apertium-sf-net/trunk/lttoolbox-java@23439 2c8c2297-680c-4500-a755-649f80e260dd

--- test/org/apertium/utils/Diff.java
+++ test/org/apertium/utils/Diff.java
@@ -7,8 +7,8 @@ public class Diff {
 	public static List<String> diff(String text1, String text2) {
 		List<String> ret = new LinkedList<String>();
 		
-		String[] x = text1.split("\\n");
-		String[] y = text2.split("\\n");
+		String[] x = text1.split("\r\n|\n|\r");
+		String[] y = text2.split("\r\n|\n|\r");
 
 		int m = x.length;
 		int n = y.length;
@@ -17,10 +17,11 @@ public class Diff {
 
 		for (int i = m - 1; i >= 0; i--) {
 			for (int j = n - 1; j >= 0; j--) {
-				if (x[i].equals(y[j]))
+				if (x[i].equals(y[j])) {
 					opt[i][j] = opt[i + 1][j + 1] + 1;
-				else
+				} else {
 					opt[i][j] = Math.max(opt[i + 1][j], opt[i][j + 1]);
+				}
 			}
 		}
 
