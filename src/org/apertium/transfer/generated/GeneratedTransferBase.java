1de447c93c11f41a46fa9b2fc05898dde8416ddb
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2011-12-29 14:24:40 +0000

    future work; currently not used
    git-svn-id: file:///home/apertium/svn-mirror/apertium-sf-net/trunk/lttoolbox-java@35562 2c8c2297-680c-4500-a755-649f80e260dd

--- src/org/apertium/transfer/generated/GeneratedTransferBase.java
+++ src/org/apertium/transfer/generated/GeneratedTransferBase.java
@@ -25,4 +25,28 @@ public abstract class GeneratedTransferBase {
   protected void logCall(String met, Object... args) {
     System.err.println("call:" + met+" "+Arrays.toString(args) );
   }
+
+
+  /**
+   * Is used by the runtime to detect obsoleted bytecode that needs to be regenerated
+   * This superclass method should return 0, as this makes old code return 0.
+   * Generated classes should define this method to return a number.
+   * Each time an inompatible change is made the number must be increased.
+   * @return The loaded class' version number. Must match runtime to be compatible,
+	future work; currently not used
+  public int getCompatibilityVersionNumber() {
+    return 0;
+  }
+   */
+
+
+  /**
+   * The runtime (not the loaded class)'s version number
+   * Each time an inompatible change is made the number must be increased.
+   * @return Version number. Must match runtime to be compatible,
+	future work; currently not used
+  public static int getRuntimeVersionNumber() {
+    return 1;
+  }
+   */
 }
